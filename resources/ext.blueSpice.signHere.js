01a37623581c50df5ee63c3a0668414cf772902e
pavloyashchenko <pyashchenko@hallowelt.com>
2023-02-14 15:20:23 +0100

    Add anouncement after button clicked
    ERM29379
    
    [4.2.x]
    
    Change-Id: I6a22d880d8809c77ece4c5069adcc00ae3638f44

--- resources/ext.blueSpice.signHere.js
+++ resources/ext.blueSpice.signHere.js
@@ -1,6 +1,6 @@
-(function( mw, $, d, undefined ){
+(function (mw, $, d, undefined) {
 
-	function _sign( pageId, targetCount ) {
+	function _sign(pageId, targetCount) {
 		var count = -1;
 		var api = new mw.Api();
 		api.get({
@@ -8,43 +8,44 @@
 			'pageids': pageId,
 			'prop': 'revisions',
 			'rvprop': 'content',
-			'indexpageids' : ''
+			'indexpageids': ''
 		})
-		.done(function( resp, jqXHR ){
-			var pageId = resp.query.pageids[0];
-			var content = resp.query.pages[pageId].revisions[0]['*'];
-			var newContent = content.replace( /<(bs:)?signhere/g, function( fullMatch, group, pos, fullString ) {
-				count++;
+			.done(function (resp, jqXHR) {
+				var pageId = resp.query.pageids[0];
+				var content = resp.query.pages[pageId].revisions[0]['*'];
+				var newContent = content.replace(/<(bs:)?signhere/g, function (fullMatch, group, pos, fullString) {
+					count++;
 
-				if( targetCount !== count ) {
-					return fullMatch;
-				}
+					if (targetCount !== count) {
+						return fullMatch;
+					}
 
-				return "--~~~~\n\n" +  fullMatch;
+					return "--~~~~\n\n" + fullMatch;
+				});
+
+				var editPageAPI = new mw.Api();
+				editPageAPI.postWithToken('csrf', {
+					'action': 'edit',
+					'pageid': pageId,
+					'text': newContent,
+					'summary': mw.message('bs-signhere-edit-summary').plain()
+				})
+					.done(function (resp, jqXHR) {
+						window.location.reload();
+						$('.signhere-alert-message')[0].innerHTML = mw.message('bs-signhere-anouncement-label').plain();
+					});
 			});
-
-			var editPageAPI = new mw.Api();
-			editPageAPI.postWithToken( 'csrf', {
-				'action': 'edit',
-				'pageid': pageId,
-				'text': newContent,
-				'summary': mw.message( 'bs-signhere-edit-summary' ).plain()
-			})
-			.done(function( resp, jqXHR ){
-				window.location.reload();
-			});
-		});
 	}
 
-	$(d).on( 'click', '.bs-signhere',  function() {
-		$(this).find( '.bs-signhere-icon' ).addClass( 'bs-signhere-spin' );
-		_sign( mw.config.get( 'wgArticleId' ), $(this).data( 'bs-counter' ) );
-	} );
-	$(d).on( 'keyup', '.bs-signhere', function( e ) {
-		if(( e.keyCode == 13 ) || ( e.keyCode == 32 )) {
-			$(this).find( '.bs-signhere-icon' ).addClass( 'bs-signhere-spin' );
-			_sign( mw.config.get( 'wgArticleId' ), $(this).data( 'bs-counter' ) );
+	$(d).on('click', '.bs-signhere', function () {
+		$(this).find('.bs-signhere-icon').addClass('bs-signhere-spin');
+		_sign(mw.config.get('wgArticleId'), $(this).data('bs-counter'));
+	});
+	$(d).on('keyup', '.bs-signhere', function (e) {
+		if ((e.keyCode == 13) || (e.keyCode == 32)) {
+			$(this).find('.bs-signhere-icon').addClass('bs-signhere-spin');
+			_sign(mw.config.get('wgArticleId'), $(this).data('bs-counter'));
 		}
-	} );
+	});
 
-})( mediaWiki, jQuery, document );
+})(mediaWiki, jQuery, document);
